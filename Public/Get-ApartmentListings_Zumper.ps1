9469a8df03c4044b1ac73db8d3c4db0168f2a65c
Nicholas Stevens <nstevens@nanick.org>
2021-04-03 01:03:36 -0500

    Update Get-ApartmentListings_Zumper.ps1

--- Public/Get-ApartmentListings_Zumper.ps1
+++ Public/Get-ApartmentListings_Zumper.ps1
@@ -1,3 +1,4 @@
+Add-Type -Path ..\lib\Execute.HttpRequest.dll
 $reg= [regex]::new(" window.__PRELOADED_STATE__ = (\{.*\})")
 $uri = "https://www.zumper.com/apartments-for-rent/chicago-il/1-beds/under-2300?bathrooms=1&polygons=}jg~FzqpuO__@ePy{A}i@ebBia@u{CePwkA?u{BhCw{@?cr@zKqXpG_OnT{nA|gAceC~cCynAtcA{~@re@ie@ha@cR?w[{KiU{KckF?|^??vXkE`]qHlTeRhCgxA?vKre@`b@vXbR`]vKvXvKnr@?|sHpHflA?tcAdBha@?nyCxKvX|NnTfxBse@t[a]lx@wXv[iC`r@?tk@hCduAdPjUiCdRqGbuB?dRiC|d[?jUia@rHwaBrX_dCrHwX?euCa_@guCy[or@sXwv@eRgn@jE{i@lEoTdB_]?a]sHen@yKse@eBa]kEwX_OwX?wXsHmTyKeP_O{KjErG&listing-amenities=2,14,7,16&box=-87.96615600585938,41.748518877483804,-87.40036010742188,42.08420992526112&images"
 $r = [Execute.HttpRequest]::Send($uri)
